yan234280533/bzflag-import-4

commit 3923a68cfc2c6794a0f69d3a14c08c8acbc29ffc

Moved remaining shots processing to the client side. The server still tracks the remaining shots and forces the client to drop the flag once the shots have been used up.
git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk@19088 08b3d480-bf2c-0410-a26f-811ee3361c24

include/bzfsAPI.h
@@ -414,7 +414,7 @@ typedef enum {
 
 typedef struct bz_PlayerUpdateState {
   bz_ePlayerStatus	status;			// special states
-  bool			falling;		// not driving on the ground or an obstacle
+  bool			falling;		// not driving on the ground or an obstafcle
   bool			crossingWall;		// crossing an obstacle wall
   bool			inPhantomZone;		// zoned
   float			pos[3];			// position of tank

include/Protocol.h
@@ -97,6 +97,7 @@ const uint16_t MsgWhatTimeIsIt      = 0x7774; // 'wt'
 const uint16_t MsgScore             = 0x7363; // 'sc'
 const uint16_t MsgScoreOver         = 0x736f; // 'so'
 const uint16_t MsgShotEnd           = 0x7365; // 'se'
+const uint16_t MsgShotsRemaining    = 0x7372; // 'sr'
 const uint16_t MsgSuperKill         = 0x736b; // 'sk'
 const uint16_t MsgSetShot           = 0x7373; // 'ss'
 const uint16_t MsgSetTeam           = 0x7374; // 'st'

src/bzflag/HUDRenderer.cxx
@@ -110,6 +110,7 @@ HUDRenderer::HUDRenderer(const BzfDisplay* _display,
   alertFontFace = LocalFontFace::create("sansSerifFont");
   majorFontFace = LocalFontFace::create("serifFont");
   minorFontFace = LocalFontFace::create("consoleFont");
+  shotsFontFace = LocalFontFace::create("sansSerifFont");
   headingFontFace = LocalFontFace::create("sansSerifFont");
   composeFontFace = LocalFontFace::create("consoleFont");
   labelsFontFace = LocalFontFace::create("consoleFont");
@@ -187,6 +188,7 @@ void			HUDRenderer::resize(bool firstTime)
   setHeadingFontSize(w, vh);
   setComposeFontSize(w, vh);
   setLabelsFontSize(w, vh);
+  setShotsFontSize(w, vh);
 
   // set scoreboard window size & location
   const float sby = window.getViewHeight() - majorFontHeight - alertFontHeight * 2.0f;
@@ -269,6 +271,12 @@ void			HUDRenderer::setLabelsFontSize(int width, int height)
 {
   FontSizer fs = FontSizer(width, height);
   labelsFontSize = fs.getFontSize(labelsFontFace->getFMFace(), "consoleFontSize");
+}
+
+void            HUDRenderer::setShotsFontSize(int width, int height)
+{
+	FontSizer fs = FontSizer(width, height);
+	shotsFontSize = fs.getFontSize(shotsFontFace->getFMFace(), "shotsFontSize");
 }
 
 void			HUDRenderer::setColor(float r, float g, float b)
@@ -1296,6 +1304,8 @@ void			HUDRenderer::renderTimes(void)
   }
 }
 
+//renders the targeting box in the hud
+
 void			HUDRenderer::renderBox(SceneRenderer&)
 {
   // get view metrics
@@ -1324,7 +1334,7 @@ void			HUDRenderer::renderBox(SceneRenderer&)
   if (true /* always draw heading strip */) {
     // first clip to area
     glScissor(ox + centerx - maxMotionSize, oy + height - viewHeight + centery + maxMotionSize - 5,
-	      2 * maxMotionSize, 25 + (int)(labelsFontSize + 0.5f));
+       2 * maxMotionSize, 25 + (int)(labelsFontSize + 0.5f));
 
     // draw heading mark
     glBegin(GL_LINES);
@@ -1605,7 +1615,7 @@ void HUDRenderer::renderPlaying(SceneRenderer& renderer)
 
   glPushMatrix();
   // cover the lower portion of the screen when burrowed
-  const LocalPlayer *myTank = LocalPlayer::getMyTank();
+  LocalPlayer *myTank = LocalPlayer::getMyTank();
   if (myTank && myTank->getPosition()[2] < 0.0f) {
     glColor4f(0.02f, 0.01f, 0.01f, 1.0);
     glRectf(0, 0, (float)width, (myTank->getPosition()[2]/(BZDB.eval(StateDatabase::BZDB_BURROWDEPTH)-0.1f)) * ((float)viewHeight/2.0f));
@@ -1657,6 +1667,7 @@ void HUDRenderer::renderPlaying(SceneRenderer& renderer)
 
   // draw targeting box
   renderBox(renderer);
+
 
   glPopMatrix();
 

src/bzflag/HUDRenderer.h
@@ -161,6 +161,7 @@ private:
   void		setHeadingFontSize(int width, int height);
   void		setComposeFontSize(int width, int height);
   void		setLabelsFontSize(int width, int height);
+  void      setShotsFontSize(int width, int height);
 
   void		resize(bool firstTime);
   static void	resizeCallback(void*);
@@ -188,6 +189,8 @@ private:
   float			majorFontSize;
   LocalFontFace*	minorFontFace;
   float			minorFontSize;
+  LocalFontFace*    shotsFontFace;
+  float         shotsFontSize;
   LocalFontFace*	headingFontFace;
   float			headingFontSize;
   LocalFontFace*	composeFontFace;

src/bzflag/LocalPlayer.cxx
@@ -33,6 +33,8 @@
 #include "SyncClock.h"
 #include "ClientIntangibilityManager.h"
 #include "MotionUtils.h"
+#include "AnsiCodes.h"
+#include "HUDRenderer.h"
 
 LocalPlayer*		LocalPlayer::mainPlayer = NULL;
 
@@ -65,7 +67,8 @@ LocalPlayer::LocalPlayer(const PlayerId& _id,
   wantJump(false),
   jumpPressed(false),
   deathPhyDrv(-1),
-  hitWall(false)
+  hitWall(false),
+  shotsRemaining(0)
 {
   // initialize shots array to no shots fired
   World *world = World::getWorld();
@@ -1025,6 +1028,11 @@ float			LocalPlayer::getFlagShakingTime() const
   return flagShakingTime;
 }
 
+int             LocalPlayer::getShotsRemaining() const
+{
+  return shotsRemaining;
+}
+
 int			LocalPlayer::getFlagShakingWins() const
 {
   return flagShakingWins;
@@ -1246,6 +1254,11 @@ bool			LocalPlayer::fireShot()
   // make shot and put it in the table
   addShot(new LocalShotPath(firingInfo,syncedClock.GetServerSeconds()), firingInfo);
 
+  // Decrement shots remaining for the current flag if there are any
+  decrementShotsRemaining();
+  if ((shotsRemaining % 5 == 0 || shotsRemaining <= 3) && shotsRemaining > 0)
+    printShotsRemaining();
+
   // Insert timestamp, useful for dead reckoning jitter fixing
   firingInfo.timeSent = (float)syncedClock.GetServerSeconds();
 
@@ -1293,6 +1306,11 @@ bool			LocalPlayer::fireShot()
     // make sure all the shots don't go off at once
     forceReload(BZDB.eval(StateDatabase::BZDB_RELOADTIME) / numShots);
   }
+
+  if(getShotsRemaining() < 1){
+    dropFlag(this);
+  }
+
   return true;
 }
 
@@ -1417,6 +1435,37 @@ void			LocalPlayer::doJump()
   }
 
   wantJump = false;
+}
+
+void            LocalPlayer::printShotsRemaining()
+{
+  if(shotsRemaining < 1)
+    return;
+  if(hud && controlPanel)
+  {
+    std::string message;
+    char shotsLeftString[10];
+    snprintf(shotsLeftString, 10, "%d", shotsRemaining);
+    message += ColorStrings[WhiteColor];
+    message += shotsLeftString;
+    message += " shot";
+    if(shotsRemaining != 1)
+      message += "s";
+    message += " left";
+    controlPanel->addMessage(message);
+    hud->setAlert(1, message.c_str(), 2.0f);
+  }
+}
+
+void            LocalPlayer::setShotsRemaining(int value)
+{
+  shotsRemaining = value;
+}
+
+void            LocalPlayer::decrementShotsRemaining()
+{
+  if(shotsRemaining > 0)
+    shotsRemaining -= 1;
 }
 
 void			LocalPlayer::setTarget(const Player* _target)
@@ -1594,6 +1643,11 @@ void			LocalPlayer::setFlag(FlagType* flag)
   World *world = World::getWorld();
   if (!world) {
     return;
+  }
+
+  if(getFlag() == Flags::Null)
+  {
+	setShotsRemaining(0);
   }
 
   float worldSize = BZDBCache::worldSize;

src/bzflag/LocalPlayer.h
@@ -80,6 +80,11 @@ public:
   void		setJumpPressed(bool value);
   void		setTarget(const Player*);
 
+  int       getShotsRemaining() const;
+  void      decrementShotsRemaining();
+  void      setShotsRemaining(int value);
+  void      printShotsRemaining();
+
   void		setDeadStop ( void  );
 
   void		setNemesis(const Player*);
@@ -163,6 +168,7 @@ private:
   bool		inputChanged;
   bool		spawning;
   int		wingsFlapCount;
+  int       shotsRemaining;
   bool		left;
   bool		right;
   bool		up;

src/bzflag/clientCommands.cxx
@@ -313,16 +313,7 @@ static std::string cmdDrop(const std::string&,
     return "usage: drop";
   LocalPlayer *myTank = LocalPlayer::getMyTank();
   if (myTank != NULL) {
-    FlagType* flag = myTank->getFlag();
-    if ((flag != Flags::Null) && !myTank->isPaused() &&
-	(flag->endurance != FlagSticky) && !myTank->isPhantomZoned() &&
-	!(flag == Flags::OscillationOverthruster &&
-	  myTank->getLocation() == LocalPlayer::InBuilding)) {
-      serverLink->sendDropFlag(myTank->getPosition());
-      // changed: on windows it may happen the MsgDropFlag
-      // never comes back to us, so we drop it right away
-      handleFlagDropped(myTank);
-    }
+    dropFlag(myTank);
   }
   return std::string();
 }

src/bzflag/defaultBZDB.cxx
@@ -153,6 +153,7 @@ DefaultDBItem	defaultDBItems[] = {
   { "alertFontSize",		"1/64",			true,	StateDatabase::ReadWrite,	NULL },
   { "infoFontSize",		"1/96",			true,	StateDatabase::ReadWrite,	NULL },
   { "consoleFontSize",		"1/128",		true,	StateDatabase::ReadWrite,	NULL },
+  { "shotsFontSize",        "1/96",         true,   StateDatabase::ReadWrite,   NULL },
   { "scoreFontSize",		"1/128",		true,	StateDatabase::ReadWrite,	NULL },
 
   // team based object sufixes

commit 3e3cb221b5094bc29f85463f5a8de07d04fac605

Remove typo that was introduced in r19088
git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk@19091 08b3d480-bf2c-0410-a26f-811ee3361c24

include/bzfsAPI.h
@@ -414,7 +414,7 @@ typedef enum {
 
 typedef struct bz_PlayerUpdateState {
   bz_ePlayerStatus	status;			// special states
-  bool			falling;		// not driving on the ground or an obstafcle
+  bool			falling;		// not driving on the ground or an obstacle
   bool			crossingWall;		// crossing an obstacle wall
   bool			inPhantomZone;		// zoned
   float			pos[3];			// position of tank